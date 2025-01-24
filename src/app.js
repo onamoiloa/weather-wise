7fca2d2161dea19b634846bbaeb5c37480d28829
onamoiloa <onalennatumelo2@gmail.com>
2025-01-25 01:29:13 +0200

    injected more weather data

--- src/app.js
+++ src/app.js
@@ -2,8 +2,18 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement =document.querySelector("#city");
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windspeedElenent = document.querySelector("#wind-speed");
+let timeElement = document.querySelector("#time");
+
+    
 
 cityElement.innerHTML = response.data.city;
+descriptionElement.innerHTML = response.data.condiion.description;
+humidityElement.innerHTML = '${response.data.temperature.humidity}%';
+windspeedElenent.innerHTML = '${response.data.wind.speed}';
+timeElement.innerHTML = "Tuesday 14:49";
     temperatureElement.innerHTML = Math.round(temperature);
     
 }
@@ -11,7 +21,7 @@ cityElement.innerHTML = response.data.city;
 function searchCity(city) {
     let apiKey= "44f618d99f8a16c0a9a9065877f062b1";
     let apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q={city}&key={44f618d99f8a16c0a9a9065877f062b1}&units=metric';
-axios.get(apiUrl).then(refreshWeather);
+
 }
 
 function handleSearchSubmit(event) {
@@ -23,3 +33,5 @@ function handleSearchSubmit(event) {
 
  let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris")
